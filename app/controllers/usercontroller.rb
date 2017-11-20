174861744fb0f9e6b58055c836309870838b3c57
Lisa Lee <snickers495@gmail.com>
2017-11-20 16:42:29 -0600

    Create add books to My Books

--- app/controllers/usercontroller.rb
+++ app/controllers/usercontroller.rb
@@ -1,9 +1,20 @@
 class UserController  < ApplicationController
-  
+
   get "/users/:slug" do
     # can view but not edit a user profile
     @user = User.find_by_slug(params[:slug])
     erb :"/users/user_profile.erb"
+  end
+
+  get "users/:slug/add" do
+    # only current user can edit their own page
+    user = User.find_by_slug(params[:slug])
+    if logged_in? && (user == current_user)
+      @user = user
+      erb :"/users/user_create"
+    else
+      redirect to "/login"
+    end
   end
 
 end
