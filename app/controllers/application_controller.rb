a55bd7c467d00532f17b0407fff66080b7eda8cd
Lisa Lee <snickers495@gmail.com>
2017-11-20 11:35:45 -0600

    made homepage and first part of controller get

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,17 @@ class ApplicationController < Sinatra::Base
     enable :sessions
 		set :session_secret, "password_security"
   end
+
+  get "/" do
+    erb :homepage
+  end
+
+  get "/signup" do
+    erb :sign_up
+  end
+
+  get "log_in" do
+    erb :log_in
+  end
+
 end
