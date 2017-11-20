c60c0f3452e41253d0bb72e3fa886ba5dfea0dd5
Lisa Lee <snickers495@gmail.com>
2017-11-20 14:13:30 -0600

    finish login, fixed slug

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
   has_many :genres, through: :books
   has_secure_password
 
-  include Slug::InstanceMethods
-  extend Slug::ClassMethods
+  def slug
+    slug = self.username.downcase.split(" ").join("-")
+    slug
+  end
+  
+  def self.find_by_slug(slug)
+    self.all.detect{ |object| object.slug == slug }
+  end
+
 end
